b345ac73a1f8f03d0cb7bba301b1f9fd99945107
saperate <61527642+saperate@users.noreply.github.com>
2026-03-09 06:53:09 -0400

    forgot to push

--- src/main/java/dev/saperate/cigs/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/dev/saperate/cigs/mixin/ServerPlayerEntityMixin.java
@@ -6,6 +6,7 @@ import dev.saperate.cigs.data.PlayerData;
 import net.fabricmc.fabric.api.entity.FakePlayer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
